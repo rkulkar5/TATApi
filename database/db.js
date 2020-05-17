4d3a03fd3940ae9e40a2ebb3a8b6ba00cd195051
rkulkar5 <rkulkar5@in.ibm.com>
2020-05-17 23:32:47 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,3 +1,4 @@
-   db: 'mongodb://admin:TATDBAdmin@1190a2a4-473b-4c03-8cd8-aded77b5122c-0.bn2a0fgd0tu045vmv2i0.databases.appdomain.cloud:31109/ibmclouddb?authSource=adminreplicaSet=replset'
+module.exports = {   
+     db: 'mongodb://admin:TATDBAdmin@1190a2a4-473b-4c03-8cd8-aded77b5122c-0.bn2a0fgd0tu045vmv2i0.databases.appdomain.cloud:31109/ibmclouddb?authSource=adminreplicaSet=replset'
    
 };
