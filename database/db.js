d6aacd89c6d7266f9fccfe557fc337c91b1608ee
rkulkar5 <rkulkar5@in.ibm.com>
2020-05-17 23:26:57 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,3 +1,3 @@
-module.exports = {
-   db: 'mongodb://admin:TATDBAdmin@1190a2a4-473b-4c03-8cd8-aded77b5122c-0.bn2a0fgd0tu045vmv2i0.databases.appdomain.cloud:31109/?authSource=admin&replicaSet=replset&readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=true'
+   db: 'mongodb://admin:TATDBAdmin@1190a2a4-473b-4c03-8cd8-aded77b5122c-0.bn2a0fgd0tu045vmv2i0.databases.appdomain.cloud:31109/ibmclouddb?authSource=adminreplicaSet=replset'
+   
 };
