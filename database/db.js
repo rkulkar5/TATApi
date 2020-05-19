552f525c973884fa262d0205a838890e0d7ca056
rkulkar5 <rkulkar5@in.ibm.com>
2020-05-19 14:50:32 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -44,6 +44,6 @@ if (credentials.hasOwnProperty("ca_certificate_base64")) {
 
 
 module.exports = {
-options: options;
+
    db:'mongodb://ibm_cloud_b5d223ef_8437_4fb0_9faa_b56b324cd57a:382425ab8920cdd7c7d24959ceda6eeb6b589f053c51c6d9a51af4bca1a592aa@1190a2a4-473b-4c03-8cd8-aded77b5122c-0.bn2a0fgd0tu045vmv2i0.databases.appdomain.cloud:31109,1190a2a4-473b-4c03-8cd8-aded77b5122c-1.bn2a0fgd0tu045vmv2i0.databases.appdomain.cloud:31109/ibmclouddb?authSource=admin&replicaSet=replset'
 };
