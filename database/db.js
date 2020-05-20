078043afa271fa7c3b6b74357f7a413868b7b68e
rkulkar5 <rkulkar5@in.ibm.com>
2020-05-20 18:37:38 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -5,7 +5,7 @@ const util = require('util')
 
 var vcapLocal;
 try {
-    vcapLocal = require('../../vcap-local.json');
+    vcapLocal = require('../vcap-local.json');
     console.log("Loaded local VCAP");
 } catch (e) {
     // console.log(e)
@@ -15,8 +15,6 @@ const appEnvOpts = vcapLocal ? { vcap: vcapLocal } : {}
 
 const appEnv = cfenv.getAppEnv(appEnvOpts);
 
-console.log("********* appEnv ************", appEnv.services);
-console.log("********* ************");
 // Within the application environment (appenv) there's a services object
 let services = appEnv.services;
 
@@ -28,23 +26,28 @@ let mongodb_services = services["compose-for-mongodb"];
 assert(!util.isUndefined(mongodb_services), "App must be bound to databases-for-mongodb service");
 
 // We now take the first bound MongoDB service and extract it's credentials object
-let credentials = mongodb_services[0].credentials;
+var credentials = mongodb_services[0].credentials;
 
-console.log("********* credentials.mongodb.certificate.certificate_base64 ************", credentials);
+console.log("********* credentials.mongodb.certificate.certificate_base64 ************", credentials.connection.mongodb.certificate.certificate_base64);
 
 // We always want to make a validated TLS/SSL connection
 let options = {
     ssl: true,
     sslValidate: true
+
 };
 
+console.log("********", credentials.connection.mongodb.certificate.hasOwnProperty("certificate_base64"));
 // If there is a certificate available, use that, otherwise assume Lets Encrypt certifications.
-if (credentials.hasOwnProperty("ca_certificate_base64")) {
-    let ca = [new Buffer(credentials.certificate.certificate_base64, 'base64')];
+if (credentials.connection.mongodb.certificate.hasOwnProperty("certificate_base64")) {
+console.log("******** INSIDE ********");
+    let ca = [new Buffer(credentials.connection.mongodb.certificate.certificate_base64, 'base64')];
     options.sslCA = ca;
+	
 }
 
-module.exports = {
+//db: 'mongodb://localhost:27017/sampleDB'
+module.exports.db = 'mongodb://admin:TATDBAdmin@1190a2a4-473b-4c03-8cd8-aded77b5122c-0.bn2a0fgd0tu045vmv2i0.databases.appdomain.cloud:31109/TATDB?authSource=admin&readPreference=primaryPreferred';
 
-   db:'mongodb://ibm_cloud_b5d223ef_8437_4fb0_9faa_b56b324cd57a:382425ab8920cdd7c7d24959ceda6eeb6b589f053c51c6d9a51af4bca1a592aa@1190a2a4-473b-4c03-8cd8-aded77b5122c-0.bn2a0fgd0tu045vmv2i0.databases.appdomain.cloud:31109,1190a2a4-473b-4c03-8cd8-aded77b5122c-1.bn2a0fgd0tu045vmv2i0.databases.appdomain.cloud:31109/ibmclouddb?authSource=admin&replicaSet=replset'
-};
+module.exports.options = options;
+
