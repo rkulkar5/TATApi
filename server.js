4b0d3114d17cf196cc348bff5e07004d5747cc55
rkulkar5 <rkulkar5@in.ibm.com>
2020-05-22 11:41:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ var corsOptions = {
 
 
 var corsOptions1 = {
-  origin: 'http://localhost:4200',
+  origin: '*',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
